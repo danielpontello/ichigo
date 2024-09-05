137e46a78763270185a22b6b19f7f37a94b492d5
sader <danielpontello@gmail.com>
2024-09-05 16:57:57 +0100

    added word list

--- question.gd
+++ question.gd
@@ -1,4 +1,4 @@
-extends Panel
+extends Control
 
 var score
 var total
---
+++ words.gd
@@ -0,0 +1,33 @@
+extends Control
+
+var data
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	data = load_words()
+	var row = load("res://row.tscn")
+	
+	for word in data:
+		var new_row = row.instantiate()
+		new_row.get_node("Title/Kanji").text = word['kanji']
+		new_row.get_node("Title/Kana").text = word['kana']
+		new_row.get_node("Title/Translation").text = word['translation']
+		$ScrollContainer/Table.add_child(new_row)
+		print(word)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func load_words():
+	var file = FileAccess.open("res://kanji.json", FileAccess.READ)
+	var content = file.get_as_text()
+	
+	var json = JSON.new()
+	var error = json.parse(content)
+	if error == OK:
+		var data = json.data
+		return data
